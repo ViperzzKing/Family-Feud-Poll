da6783935df89b8d8ba08c18444b2ccf532f5c03
ViperzzKing <viperzzkingg@gmail.com>
2025-08-28 13:35:39 +1000

    made backend

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -1,16 +1,58 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class DataManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    //TODO -- Update this list everytime your user answers relevant question
+    public List<Question> myQuestions;
+    //TODO -- Use these varibles in the assignment
+    public Question currentQuestion;
+    public int index = 0;
+
+    private void Start()
     {
-        JSON_Manager.SayHello();
+        LoadGame();
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Creates a SavedData file and give it all the questions in 'myQuestions'
+    /// Encrypt that into a JSON file with the JSON_Manager
+    /// </summary>
+    [ContextMenu("Save Game")]
+    public void SaveGame()
     {
+        Debug.Log("Saving Game...");
+        SavedData savedData = new SavedData();
+
+        savedData.savedQuestions = myQuestions;  
         
+        JSON_Manager.SaveToJSON(savedData);
     }
+
+    /// <summary>
+    /// Use the JSON_Manager to retreive the questions stored away
+    /// Push them into 'myQuestions' list
+    /// If the load fails, save the game instead to make a new file
+    /// </summary>
+    [ContextMenu("Load Game")]
+    public void LoadGame()
+    {
+        Debug.Log("Loading Game...");
+        SavedData loadedData = JSON_Manager.LoadFromJSON();
+        
+        if(loadedData != null)
+            myQuestions = loadedData.savedQuestions;
+        else
+            SaveGame();
+    }
+}
+
+[Serializable]
+
+// Can be a new script
+public class Question // Stores the Questions & Responses
+{
+    public string question;
+    public List<string> responses;
 }
--- Assets/Scripts/JSON_Manager.cs
+++ Assets/Scripts/JSON_Manager.cs
@@ -1,29 +1,55 @@
+using System;
+using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
 public static class JSON_Manager
 {
-    public static void SayHello()
-    {
-        Debug.Log("Hello!");
-    }
-
-    public static void SaveToJSON()
+    
+    /// <summary>
+    /// We use this JSON Encryption algorithim to take a object (SavedData) and push it into a Text/JSON file
+    /// CTRL-V in file browser to see where its saved
+    /// </summary>
+    /// <param name="data"></param>
+    public static void SaveToJSON(SavedData data)
     {
         string directory = Application.persistentDataPath + "/SavedData/";
+
+        if (!Directory.Exists(directory)) Directory.CreateDirectory(directory);
+        
+        string jsonEncryption = JsonUtility.ToJson(data, true);
+        
+        File.WriteAllText(directory + "responseDataBase.json", jsonEncryption);
+        GUIUtility.systemCopyBuffer = directory; // CTRL-C
     }
     
-    public static void LoadToJSON()
+    /// <summary>
+    /// We do this so we can turn a Text/JSON file into a object(SavedData) which will be returned
+    /// Returns "Null" if it cant find a file
+    /// </summary>
+    /// <returns></returns>
+    public static SavedData LoadFromJSON()
     {
-        string directory = Application.persistentDataPath + "/SavedData/" + "responseDataBase";
+        string directory = Application.persistentDataPath + "/SavedData/" + "responseDataBase.json";
 
         if (File.Exists(directory))
         {
-            //TODO -- THIS IS THE FUN STUFF!!!
+            string json = File.ReadAllText(directory);
+            return JsonUtility.FromJson<SavedData>(json);
         }
         else
         {
             Debug.Log("Load Failed. No File Found");
+            return null;
         }
     }
+}
+
+[Serializable] // Show in Inspector
+
+// This class can be its own script
+public class SavedData // This way we can collect information you can save when prompted
+{
+    // Can save any varible here were saving a list
+    public List<Question> savedQuestions;
 }
